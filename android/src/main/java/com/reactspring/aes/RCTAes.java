a46b9969f7e50bc36bd662372b127a90866665b7
moistbobo <moistbobo@outlook.com>
2021-01-04 14:01:29 +0800

    refactor: use alternate android implementation

--- android/src/main/java/com/reactspring/aes/RCTAes.java
+++ android/src/main/java/com/reactspring/aes/RCTAes.java
@@ -1,27 +1,19 @@
 package com.reactspring.aes;
 
-import java.io.UnsupportedEncodingException;
-import java.security.SecureRandom;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
+import android.util.Log;
 
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-import javax.crypto.spec.IvParameterSpec;
-
-import org.spongycastle.crypto.digests.SHA512Digest;
-import org.spongycastle.crypto.generators.PKCS5S2ParametersGenerator;
-import org.spongycastle.crypto.params.KeyParameter;
-import org.spongycastle.util.encoders.Hex;
-
-import android.util.Base64;
-
-import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
 
 public class RCTAes extends ReactContextBaseJavaModule {
     private static final String CIPHER_ALGORITHM = "AES/CBC/PKCS7Padding";
@@ -36,97 +28,66 @@ public class RCTAes extends ReactContextBaseJavaModule {
         return "RCTAes";
     }
 
-    @ReactMethod
-    public void encrypt(String data, String key, String iv, Promise promise) {
+
+@ReactMethod
+    public void encrypt(String sSrc, String secretKey, String ivParameter, Promise promise) {
         try {
-            String result = encrypt(data, key, iv);
-            promise.resolve(result);
+            if (secretKey == null && "".equals(secretKey)) {
+                Log.e("secretKey====>>>>", "secretKey is null");
+                promise.reject("INVALID_PARAMETER", "secretKey MUST not be null");
+            }
+            if (ivParameter == null && "".equals(ivParameter)) {
+                Log.e("ivParameter====>>>>", "ivParameter is null");
+                promise.reject("INVALID_PARAMETER", "ivParameter MUST not be null");
+                return;
+            }
+            byte[] encrypted = null;
+            try {
+                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+                byte[] raw = secretKey.getBytes();
+                SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+                IvParameterSpec iv = new IvParameterSpec(ivParameter.getBytes());// 使用CBC模式，需要一个向量iv，可增加加密算法的强度
+                cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+                encrypted = cipher.doFinal(sSrc.getBytes("utf-8"));
+            } catch (Exception e) {
+                e.printStackTrace();
+                promise.reject(e);
+            }
+            String cipher = new BASE64Encoder().encode(encrypted);
+            promise.resolve(cipher);
         } catch (Exception e) {
-            promise.reject("-1", e.getMessage());
+            e.printStackTrace();
+            promise.reject(e);
         }
     }
 
     @ReactMethod
-    public void decrypt(String data, String pwd, String iv, Promise promise) {
+    public void decrypt(String sSrc, String secretKey, String ivParameter, Promise promise) {
         try {
-            String strs = decrypt(data, pwd, iv);
-            promise.resolve(strs);
+            if (secretKey == null && "".equals(secretKey)) {
+                Log.e("secretKey====>>>>", "secretKey is null");
+                promise.reject("INVALID_PARAMETER", "secretKey MUST not be null");
+            }
+            if (ivParameter == null && "".equals(ivParameter)) {
+                Log.e("ivParameter====>>>>", "ivParameter is null");
+                promise.reject("INVALID_PARAMETER", "ivParameter MUST not be null");
+                return;
+            }
+            byte[] raw = secretKey.getBytes("ASCII");
+            SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            IvParameterSpec iv = new IvParameterSpec(ivParameter.getBytes());
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+
+            byte[] encrypted1 = new BASE64Decoder().decodeBuffer(sSrc);// 先用base64解密
+
+            byte[] original = cipher.doFinal(encrypted1);
+            String originalString = new String(original, "utf-8");
+            promise.resolve(originalString);
+
         } catch (Exception e) {
-            promise.reject("-1", e.getMessage());
+            e.printStackTrace();
+            promise.reject(e);
         }
     }
-
-    @ReactMethod
-    public void pbkdf2(String pwd, String salt, Integer cost, Integer length, Promise promise) {
-        try {
-            String strs = pbkdf2(pwd, salt, cost, length);
-            promise.resolve(strs);
-        } catch (Exception e) {
-            promise.reject("-1", e.getMessage());
-        }
-    }
-
-    @ReactMethod
-    public void randomKey(Integer length, Promise promise) {
-        try {
-            byte[] key = new byte[length];
-            SecureRandom rand = new SecureRandom();
-            rand.nextBytes(key);
-            String keyHex = bytesToHex(key);
-            promise.resolve(keyHex);
-        } catch (Exception e) {
-            promise.reject("-1", e.getMessage());
-        }
-    }
-
-    public static String bytesToHex(byte[] bytes) {
-        final char[] hexArray = "0123456789abcdef".toCharArray();
-        char[] hexChars = new char[bytes.length * 2];
-        for ( int j = 0; j < bytes.length; j++ ) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = hexArray[v >>> 4];
-            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-        }
-        return new String(hexChars);
-    }
-
-    private static String pbkdf2(String pwd, String salt, Integer cost, Integer length)
-    throws NoSuchAlgorithmException, InvalidKeySpecException, UnsupportedEncodingException
-    {
-        PKCS5S2ParametersGenerator gen = new PKCS5S2ParametersGenerator(new SHA512Digest());
-        gen.init(pwd.getBytes("UTF_8"), salt.getBytes("UTF_8"), cost);
-        byte[] key = ((KeyParameter) gen.generateDerivedParameters(length)).getKey();
-        return bytesToHex(key);
-    }
-
-    final static IvParameterSpec emptyIvSpec = new IvParameterSpec(new byte[] {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00});
-
-    private static String encrypt(String text, String hexKey, String hexIv) throws Exception {
-        if (text == null || text.length() == 0) {
-            return null;
-        }
-
-        byte[] key = Hex.decode(hexKey);
-        SecretKey secretKey = new SecretKeySpec(key, KEY_ALGORITHM);
-
-        Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey, hexIv == null ? emptyIvSpec : new IvParameterSpec(Hex.decode(hexIv)));
-        byte[] encrypted = cipher.doFinal(text.getBytes("UTF-8"));
-        return Base64.encodeToString(encrypted, Base64.NO_WRAP);
-    }
-
-    private static String decrypt(String ciphertext, String hexKey, String hexIv) throws Exception {
-        if(ciphertext == null || ciphertext.length() == 0) {
-            return null;
-        }
-
-        byte[] key = Hex.decode(hexKey);
-        SecretKey secretKey = new SecretKeySpec(key, KEY_ALGORITHM);
-
-        Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
-        cipher.init(Cipher.DECRYPT_MODE, secretKey, hexIv == null ? emptyIvSpec : new IvParameterSpec(Hex.decode(hexIv)));
-        byte[] decrypted = cipher.doFinal(Base64.decode(ciphertext, Base64.NO_WRAP));
-        return new String(decrypted, "UTF-8");
-    }
-
 }
